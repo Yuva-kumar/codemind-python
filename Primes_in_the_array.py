11463efce5e574e6236f58a0a9c98a37549eefa4
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2023-06-04 12:55:47 +0530

    import math def prime(n):     a=int(math.sqrt(n))     if n==1:         return False     for i in range(2,a+1):         if n%i ==0:             return False     return True n=int(input()) lst=list(map(int,input().split())) l=[] for i in lst:     if prime(i):         l.append(i) print(len(l))

---
+++ Primes_in_the_array.py
@@ -0,0 +1,16 @@
+import math
+def prime(n):
+    a=int(math.sqrt(n))
+    if n==1:
+        return False
+    for i in range(2,a+1):
+        if n%i ==0:
+            return False
+    return True
+n=int(input())
+lst=list(map(int,input().split()))
+l=[]
+for i in lst:
+    if prime(i):
+        l.append(i)
+print(len(l))
