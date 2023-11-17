3cea666d9c7eae6fa00d6074a0875e110fda0e7d
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2023-11-17 18:10:53 +0530

    def pal(n):     if str(n)==str(n)[::-1]:         return True     else:         return False n=int(input()) l=[] for i in range(0,(n*2)+100):     if pal(i):         l.append(i) l1,l2=[],[] for i in l:     if i<n:         l1.append(i)     elif i>n:         l2.append(i) a=abs(l1[-1]-n) b=abs(l2[0]-n) if a==b:     print(l1[-1],l2[0]) elif a>b:     print(l2[0]) else:     print(l1[-1])

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,26 +1,25 @@
 def pal(n):
-    a=str(n)
-    if n!=(int(a[::-1])):
-        return False
-    else:
+    if str(n)==str(n)[::-1]:
         return True
+    else:
+        return False
 n=int(input())
 l=[]
-l1=[]
-l2=[]
-for i in range(n*2):
+for i in range(0,(n*2)+100):
     if pal(i):
         l.append(i)
+l1,l2=[],[]
 for i in l:
     if i<n:
         l1.append(i)
     elif i>n:
         l2.append(i)
-a=l1[-1]
-b=l2[0]
-if abs(n-a)>abs(n-b):
-    print(b)
-elif abs(n-a)==abs(n-b):
-    print(a,b)
+a=abs(l1[-1]-n)
+b=abs(l2[0]-n)
+if a==b:
+    print(l1[-1],l2[0])
+elif a>b:
+    print(l2[0])
 else:
-    print(a)
+    print(l1[-1])
+
