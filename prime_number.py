b4e887630e33a3e87ed8ad6ffd48c8f75ccf7bee
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2023-11-17 18:21:37 +0530

    def prime(n):     a=int(n**0.5)     if n<2:         return False     for i in range(2,a+1):         if n%i==0:             return False     return True n=int(input()) if prime(n):     print('prime') else:     print('not a prime')

--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,13 @@
-import math
-def fun(n):
-    a=math.sqrt(n)
-    b=int(a)
-    for i in range(2,b+1):
+def prime(n):
+    a=int(n**0.5)
+    if n<2:
+        return False
+    for i in range(2,a+1):
         if n%i==0:
             return False
     return True
 n=int(input())
-fun(n)
-if fun(n):
+if prime(n):
     print('prime')
 else:
     print('not a prime')
